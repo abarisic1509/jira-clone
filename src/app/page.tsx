32b1bff4b13fa881d4c9859f31d7bdffd2307134
abarisic1509 <abarisic@protonmail.com>
2024-10-09 17:40:41 +0200

    components customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
 	return (
-		<div className="w-screen h-screen bg-green-400">
-			<Button>Button</Button>
+		<div className="w-screen h-screen bg-neutral-100 bg-opacity-15 py-20 px-10 flex gap-4 flex-wrap">
+			<Button size={"sm"}>Primary</Button>
+			<Button variant={"secondary"}>Secondary</Button>
+			<Button variant={"outline"}>Outline</Button>
+			<Button variant={"ghost"}>Ghost</Button>
+			<Button variant={"muted"}>Muted</Button>
+			<Button variant={"destructive"}>Destructive</Button>
+			<Button variant={"tetriary"}>Tetriary</Button>
 		</div>
 	);
 }
